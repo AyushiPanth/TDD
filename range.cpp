fd2871a8c3b7066b4a528eb52c092decc6ef9453
Titiksha Sharma <titiksha1sharma@gmail.com>
2021-01-19 17:38:07 +0530

    added empty class structure

---
+++ range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class range{
+	int start;
+	int end; 
+	
+	public:
+	range(int start, int end){
+		this -> start = start;
+		this -> end = end;
+		}
+};
